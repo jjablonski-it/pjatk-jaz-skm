e50b583664d6eba26163c1dc796037cdab60dacc
Jakub Jabłoński <jjablonski.it@gmail.com>
2020-10-25 17:19:12 +0100

    api basic setup

---
+++ skm-api/src/main/java/pl/pjatk/skmapi/SkmApiApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.skmapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkmApiApplication.class, args);
+	}
+
+}
