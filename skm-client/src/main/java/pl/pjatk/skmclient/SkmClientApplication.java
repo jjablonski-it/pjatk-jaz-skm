0e92fbf711624136759b88f7257e6d9b207d5c60
Jakub Jabłoński <jjablonski.it@gmail.com>
2020-10-26 14:33:38 +0100

    added client template

---
+++ skm-client/src/main/java/pl/pjatk/skmclient/SkmClientApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.skmclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkmClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkmClientApplication.class, args);
+	}
+
+}
---
+++ skm-client/src/test/java/pl/pjatk/skmclient/SkmClientApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.skmclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkmClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
