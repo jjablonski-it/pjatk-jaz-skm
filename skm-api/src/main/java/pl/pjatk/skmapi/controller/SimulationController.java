f75fb15dabacfa79398c2ee1c03b78845ae94ee3
Jakub Jabłoński <jjablonski.it@gmail.com>
2020-12-22 13:52:32 +0100

    simulation controller test done

--- skm-api/src/main/java/pl/pjatk/skmapi/controller/SimulationController.java
+++ skm-api/src/main/java/pl/pjatk/skmapi/controller/SimulationController.java
@@ -1,6 +1,8 @@
 package pl.pjatk.skmapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.pjatk.skmapi.service.SimulationService;
@@ -15,7 +17,13 @@ public class SimulationController {
     }
 
     @PostMapping("/move")
-    public void move(){
-        simulationService.move();
+    public ResponseEntity move() {
+        try {
+            simulationService.move();
+            return new ResponseEntity(HttpStatus.OK
+            );
+        } catch (Exception e) {
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
---
+++ skm-api/src/test/java/pl/pjatk/skmapi/Mvc/SimulationControllerTest.java
@@ -0,0 +1,21 @@
+package pl.pjatk.skmapi.Mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SimulationControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void move() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/move")).andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
