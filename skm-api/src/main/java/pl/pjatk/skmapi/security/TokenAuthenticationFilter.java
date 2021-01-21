b75b8ee1b02a3241d1082d2d7b9c923981952b2e
Jakub Jabłoński <jjablonski.it@gmail.com>
2021-01-21 12:15:08 +0100

    added TokenAuthenticationFilter

---
+++ skm-api/src/main/java/pl/pjatk/skmapi/security/TokenAuthenticationFilter.java
@@ -0,0 +1,33 @@
+package pl.pjatk.skmapi.security;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TokenAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    AuthenticationManager authenticationManager;
+
+    public TokenAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+        setFilterProcessesUrl("/login");
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        System.out.println("attemptAuthentication");
+        return super.attemptAuthentication(request, response);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        System.out.println("attemptAuthentication");
+        super.successfulAuthentication(request, response, chain, authResult);
+    }
+}
