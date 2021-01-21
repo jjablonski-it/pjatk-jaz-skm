7d367e6c1ae27092f8ac76f791c5c50bb2fafab5
Jakub Jabłoński <jjablonski.it@gmail.com>
2021-01-21 11:20:00 +0100

    added UserDetailsService implementation

--- skm-api/src/main/java/pl/pjatk/skmapi/service/UserService.java
+++ skm-api/src/main/java/pl/pjatk/skmapi/service/UserService.java
@@ -1,18 +1,35 @@
 package pl.pjatk.skmapi.service;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.pjatk.skmapi.exception.NotFoundException;
 import pl.pjatk.skmapi.model.User;
 
 @Service
-public class UserService extends CrudService<User>{
-    public UserService(JpaRepository<User, Long> repository) {
+public class UserService extends CrudService<User> implements UserDetailsService {
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(JpaRepository<User, Long> repository, PasswordEncoder passwordEncoder) {
         super(repository);
+        this.passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     @Override
     public User getUpdatedEntity(User user) throws NotFoundException {
         return null;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findAll().stream().filter(user -> user.getUsername().equals(username)).findAny().orElseThrow(() -> new UsernameNotFoundException("User " + username + " does not exist"));
+    }
+
+    public PasswordEncoder getPasswordEncoder() {
+        return passwordEncoder;
     }
 }
