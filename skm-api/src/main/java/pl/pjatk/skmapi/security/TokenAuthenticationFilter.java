7a943991ee5a76c7fefd41ac40de1fd28ef5c589
Jakub Jabłoński <jjablonski.it@gmail.com>
2021-01-21 14:33:35 +0100

    added successfulAuthentication logic

--- skm-api/src/main/java/pl/pjatk/skmapi/security/TokenAuthenticationFilter.java
+++ skm-api/src/main/java/pl/pjatk/skmapi/security/TokenAuthenticationFilter.java
@@ -1,21 +1,25 @@
 package pl.pjatk.skmapi.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import pl.pjatk.skmapi.model.User;
-import pl.pjatk.skmapi.repository.UserRepository;
-import pl.pjatk.skmapi.service.UserService;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+import static pl.pjatk.skmapi.security.util.SecurityConstants.EXPIRATION_TIME;
+import static pl.pjatk.skmapi.security.util.SecurityConstants.SECRET;
 
 public class TokenAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     AuthenticationManager authenticationManager;
@@ -30,7 +34,7 @@ public class TokenAuthenticationFilter extends UsernamePasswordAuthenticationFil
         System.out.println("attemptAuthentication");
         try {
             User user = new ObjectMapper().readValue(request.getInputStream(), User.class);
-            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()); // ?
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword());
             return authenticationManager.authenticate(authenticationToken);
         } catch (Exception e) {
             e.printStackTrace();
@@ -40,7 +44,18 @@ public class TokenAuthenticationFilter extends UsernamePasswordAuthenticationFil
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        System.out.println("successfulAuthentication");
-        super.successfulAuthentication(request, response, chain, authResult);
+        String subject = authResult.getName();
+        var authorities = authResult.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList());
+        System.out.println("authorities "+ authorities);
+
+        String token = JWT.create()
+                .withSubject(subject)
+                .withClaim("authorities", authorities)
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .sign(Algorithm.HMAC512(SECRET));
+
+        String body = subject + " " + token;
+        response.getWriter().write(body);
+        response.getWriter().flush();
     }
 }
