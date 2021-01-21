e82845b3f2aae3d36dcd7d1c97b861b457ebeccc
Jakub Jabłoński <jjablonski.it@gmail.com>
2021-01-21 14:05:10 +0100

    changed WebSecurityConfig for testing, attempting authentication works|

--- skm-api/src/main/java/pl/pjatk/skmapi/security/WebSecurityConfig.java
+++ skm-api/src/main/java/pl/pjatk/skmapi/security/WebSecurityConfig.java
@@ -4,12 +4,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/**").permitAll().and().csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .antMatchers("/user").hasAuthority("ROLE_USER").anyRequest().authenticated()
+                .and().addFilter(new TokenAuthenticationFilter(authenticationManager()))
+                .sessionManagement()
+                .and().csrf().disable();
     }
 }
