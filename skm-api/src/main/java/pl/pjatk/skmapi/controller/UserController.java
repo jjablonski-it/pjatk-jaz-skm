8f38b36d6f5623c65a12f587875edafdc684fe4b
Jakub Jabłoński <jjablonski.it@gmail.com>
2021-01-22 18:32:08 +0100

    no time, no dry

--- skm-api/src/main/java/pl/pjatk/skmapi/controller/UserController.java
+++ skm-api/src/main/java/pl/pjatk/skmapi/controller/UserController.java
@@ -1,12 +1,16 @@
 package pl.pjatk.skmapi.controller;
 
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.pjatk.skmapi.model.User;
 import pl.pjatk.skmapi.repository.UserRepository;
 import pl.pjatk.skmapi.service.CrudService;
 import pl.pjatk.skmapi.service.UserService;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -18,27 +22,41 @@ public class UserController extends CrudController<User> {
         this.userService = service;
     }
 
+    // No time, no DRY
     @PostMapping("/{id}/auth/{auth}")
-    public void addAuth(@PathVariable Long id, @PathVariable String auth) {
-        var user = userService.getById(id);
-        user.addAuthority(() -> auth);
-        userService.save(user);
+    public ResponseEntity addAuth(@PathVariable Long id, @PathVariable String auth) {
+        try {
+            User user = userService.getById(id);
+            user.addAuthority(() -> auth);
+            userService.save(user);
+            return new ResponseEntity(HttpStatus.ACCEPTED);
+        } catch (Exception e) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
     }
 
     @PutMapping("/{id}/auth/{auth}")
-    public void setAuth(@PathVariable Long id, @PathVariable String auth) {
-        var user = userService.getById(id);
-        user.setStringAuthority(auth);
-        userService.save(user);
+    public ResponseEntity setAuth(@PathVariable Long id, @PathVariable String auth) {
+        try {
+            User user = userService.getById(id);
+            user.setStringAuthority(auth);
+            userService.save(user);
+            return new ResponseEntity(HttpStatus.ACCEPTED);
+        } catch (Exception e) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
 
     }
 
     @DeleteMapping("/{id}/auth/{auth}")
-    public void deleteAuth(@PathVariable Long id, @PathVariable String auth) {
-        var user = userService.getById(id);
-        user.removeAuthority(() -> auth);
-        userService.save(user);
+    public ResponseEntity deleteAuth(@PathVariable Long id, @PathVariable String auth) {
+        try {
+            User user = userService.getById(id);
+            user.removeAuthority(() -> auth);
+            userService.save(user);
+            return new ResponseEntity(HttpStatus.ACCEPTED);
+        } catch (Exception e) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
     }
-
-
 }
