c68703bf99d74b36fb2c487153d3c273406f554a
Jakub Jabłoński <jjablonski.it@gmail.com>
2021-01-22 17:25:57 +0100

    added section test for user

--- skm-api/src/test/java/pl/pjatk/skmapi/Controller/Section/SectionControllerTestPrivileged.java
+++ skm-api/src/test/java/pl/pjatk/skmapi/Controller/Section/SectionControllerTestPrivileged.java
@@ -1,6 +1,8 @@
-package pl.pjatk.skmapi.Controller;
+package pl.pjatk.skmapi.Controller.Section;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,30 +11,51 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static pl.pjatk.skmapi.security.util.SecurityConstants.HEADER_STRING;
+import static pl.pjatk.skmapi.security.util.SecurityConstants.TOKEN_PREFIX;
+
 @SpringBootTest
 @AutoConfigureMockMvc
-public class SectionControllerTest {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class SectionControllerTestPrivileged {
     @Autowired
     MockMvc mockMvc;
 
+    String TOKEN;
+
+    @BeforeAll
+    public void setUp() throws Exception {
+        var response = mockMvc.perform(MockMvcRequestBuilders.get("/login").contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                           {
+                             "login": "privileged",
+                             "password": "123"
+                           }
+                        """)).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+        String content = response.getResponse().getContentAsString();
+        String token = content.split(" ")[1];
+        this.TOKEN = TOKEN_PREFIX + token;
+    }
+
     @Test
     public void getAll() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/section")).andExpect(MockMvcResultMatchers.status().isOk());
+        mockMvc.perform(MockMvcRequestBuilders.get("/section").header(HEADER_STRING, TOKEN)).andExpect(MockMvcResultMatchers.status().isOk());
     }
 
     @Test
     public void getById() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/section/1")).andExpect(MockMvcResultMatchers.status().isOk());
+        mockMvc.perform(MockMvcRequestBuilders.get("/section/1").header(HEADER_STRING, TOKEN)).andExpect(MockMvcResultMatchers.status().isOk());
     }
 
     @Test
     public void getByIdNotFound() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/section/99")).andExpect(MockMvcResultMatchers.status().isNotFound());
+        mockMvc.perform(MockMvcRequestBuilders.get("/section/99").header(HEADER_STRING, TOKEN)).andExpect(MockMvcResultMatchers.status().isNotFound());
     }
 
     @Test
     public void add() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.post("/section")
+                .header(HEADER_STRING, TOKEN)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("""
                            {
@@ -43,12 +66,13 @@ public class SectionControllerTest {
                                "people": []
                            }
                         """))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
     }
 
     @Test
     public void addBadRequest() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.post("/section")
+                .header(HEADER_STRING, TOKEN)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("""
                            {
@@ -56,22 +80,23 @@ public class SectionControllerTest {
                                "people": []
                            }
                         """))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
     }
 
     @Test
     public void deleteById() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.delete("/section/1")).andExpect(MockMvcResultMatchers.status().isOk());
+        mockMvc.perform(MockMvcRequestBuilders.delete("/section/1").header(HEADER_STRING, TOKEN)).andExpect(MockMvcResultMatchers.status().isForbidden());
     }
 
     @Test
     public void deleteByIdNotFound() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.delete("/section/99")).andExpect(MockMvcResultMatchers.status().isNotFound());
+        mockMvc.perform(MockMvcRequestBuilders.delete("/section/99").header(HEADER_STRING, TOKEN)).andExpect(MockMvcResultMatchers.status().isForbidden());
     }
 
     @Test
     public void update() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.put("/section")
+                .header(HEADER_STRING, TOKEN)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("""
                            {
@@ -79,31 +104,33 @@ public class SectionControllerTest {
                                "maxSeats": 1
                            }
                         """))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
     }
 
     @Test
     public void updateNotFound() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.put("/section")
                 .contentType(MediaType.APPLICATION_JSON)
+                .header(HEADER_STRING, TOKEN)
                 .content("""
                            {
                                "id": 99,
                                "maxSeats": 1
                            }
                         """))
-                .andExpect(MockMvcResultMatchers.status().isNotFound());
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
     }
 
     @Test
     public void updateBadRequest() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.put("/section")
+                .header(HEADER_STRING, TOKEN)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("""
                            {
                                "maxSeats": 1
                            }
                         """))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
     }
 }
---
+++ skm-api/src/test/java/pl/pjatk/skmapi/Controller/Section/SectionControllerTestUnauthorized.java
@@ -0,0 +1,69 @@
+package pl.pjatk.skmapi.Controller.Section;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static pl.pjatk.skmapi.security.util.SecurityConstants.HEADER_STRING;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SectionControllerTestUnauthorized {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void getAllNotAuthorized() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/section")).andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+    @Test
+    public void getByIdNotAuthorized() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/section/1")).andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+    @Test
+    public void getByIdNotFoundNotAuthorized() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/section/99")).andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+    @Test
+    public void addNotAuthorized() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/section")).andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+    @Test
+    public void addBadRequestNotAuthorized() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/section")).andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+    @Test
+    public void deleteByIdNotAuthorized() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/section/1")).andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+    @Test
+    public void deleteByIdNotFoundNotAuthorized() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/section/99")).andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+    @Test
+    public void updateNotAuthorized() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.put("/section")).andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+    @Test
+    public void updateNotFoundNotAuthorized() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.put("/section")).andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+    @Test
+    public void updateBadRequestNotAuthorized() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.put("/section")).andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+}
